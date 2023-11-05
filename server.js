1bd9531d567fcfee49b6dfc6447a16982e5c20f3
armaghan-behlum <armaghan.behlum@akasa.com>
2023-11-05 11:40:01 -0800

    Cleanup UX to make Query Explorer

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-let http = require('http');
-let cors = require('cors');
-var express = require('express');
+let http = require("http");
+let cors = require("cors");
+var express = require("express");
 var app = express();
 app.use(cors());
-const fshModule = require("./serverModuleFSH.js")
+const fshModule = require("./serverModuleFSH.js");
 
-process.on('uncaughtException', function(err) {
-    console.log('Caught exception: ' + err);
+process.on("uncaughtException", function (err) {
+  console.log("Caught exception: " + err);
 });
 
 var port = process.env.port || 8000;
 let server = http.createServer(app).listen(port);
 console.log(`listening on port ${port}`);
 
-fshModule.setup(app)
+fshModule.setup(app);
 
-app.use('/', express.static(__dirname,{index:'/query.html'}));
+app.use("/", express.static(__dirname, { index: "/query.html" }));
